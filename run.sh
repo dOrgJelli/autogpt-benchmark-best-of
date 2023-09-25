a55d061d59418584706fc6514ab796e3b70d5507
dOrgJelli <jelli@dorg.tech>
2023-09-26 01:19:11 +0200

    chore: fix run script

--- run.sh
+++ run.sh
@@ -4,12 +4,14 @@ mv ./AutoGPT/benchmark ./benchmark
 rm -rf AutoGPT
 
 # Extract relevant folders
+rm -rf ./challenges
 mv ./benchmark/agbenchmark/challenges ./challenges
 mv ./benchmark/reports ./reports
 rm -rf ./benchmark
 
 # Prune failed tests
 yarn prune-failed-tests
+rm -rf ./reports
 
 # Extract best times
 yarn extract-best-of
